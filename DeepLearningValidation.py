e50c7f56876395e2c35405c48a8badbf93e637e7
Longquan Chen <leochan2009@hotmail.com>
2017-04-11 20:36:11 -0400

    A deeplearning segmentation client
    A deeplearning segmentation client
    
    A deeplearning segmentation client

--- DeepLearningValidation.py
+++ DeepLearningValidation.py
@@ -1,13 +1,25 @@
-import subprocess
-
-def RunValidataion(inputVolumeFileName, outputLableFileName):
-	tempDir = os.path.join(os.path.realpath(__file__), '.tmp')
-	deepInferDir = '/home/deepinfer/data'
-	cmd = []
-	cmd.extend(('/usr/local/bin/docker', 'run', '-t', '-v')) # TODO: adapt for Windows
-	cmd.append(tempDir + ':' + deepInferDir)
-	cmd.append('deepinfer/prostate-segmenter-cpu')
-	cmd.extend(("--InputVolume", os.path.join(deepInferDir, inputVolumeFileName)))
-	cmd.extend(("--OutputLabel", os.path.join(deepInferDir, outputLabelFileName)))
-	p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
-	
+import subprocess, os
+import shutil
+class ClientValidationObject(object):
+  def __init__(self):
+    pass
+  def RunValidataion(self, inputVolumeFile, outputLabelFile):
+    executeDir = os.path.join(os.path.abspath(os.path.join(inputVolumeFile, os.pardir)), '.tmp')
+    if not os.path.exists(executeDir):
+      os.mkdir(executeDir)
+    shutil.copyfile(inputVolumeFile, os.path.join(executeDir,"MRProstate.nrrd"))
+    deepInferDir = '/home/deepinfer/data'
+    cmd = []
+    cmd.extend(('/usr/local/bin/docker', 'run', '-t', '-v'))
+    cmd.append(executeDir + ':' + deepInferDir)
+    cmd.append('deepinfer/prostate-segmenter-cpu')
+    cmd.extend(("--InputVolume", os.path.join(deepInferDir, "MRProstate.nrrd")))
+    cmd.extend(("--OutputLabel", os.path.join(deepInferDir, "ResultLabel.nrrd")))
+    print "into validation", cmd
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+    for line in p.stdout:
+      print line
+    p.wait()
+    shutil.copyfile(os.path.join(executeDir, "ResultLabel.nrrd"), outputLabelFile)
+    shutil.rmtree(executeDir)
+    print "validataion finished"
